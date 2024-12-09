24a0f3244966a0522e88e65f2ab6021088a6e358
boutamente-abdessamad <boutamente.abdessamad@gmail.com>
2024-12-09 10:44:56 +0100

    finish static pages

---
+++ js/main.js
@@ -0,0 +1,93 @@
+'use strict';
+(function ($) {
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        appendTo: '.header-section',
+        allowParentLinks: true,
+        closedSymbol: '<i class="fa fa-angle-right"></i>',
+		openedSymbol: '<i class="fa fa-angle-down"></i>'
+    });
+
+
+    /*------------------
+        Carousel Slider
+    --------------------*/
+     $(".hero-items").owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: true,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        smartSpeed: 1200,
+        autoplayHoverPause: true,
+        mouseDrag: false,
+        autoplay: false,
+    });
+
+
+
+    /*------------------
+        Carousel Slider
+    --------------------*/
+    $(".product-slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        items: 1,
+        dots: true,
+        autoplay: true,
+    });
+
+    /*-------------------
+		Quantity change
+	--------------------- */
+    var proQty = $('.pro-qty');
+    proQty.prepend('<span class="dec qtybtn">-</span>');
+    proQty.append('<span class="inc qtybtn">+</span>');
+    proQty.on('click', '.qtybtn', function () {
+        var $button = $(this);
+        var oldValue = $button.parent().find('input').val();
+        if ($button.hasClass('inc')) {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        $button.parent().find('input').val(newVal);
+    });
+
+    /*-------------------
+		Radio Btn
+	--------------------- */
+    $(".shipping-info .cs-item label").on('click', function () {
+        $(".shipping-info .cs-item label").removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $(".checkout-form .diff-addr label").on('click', function () {
+        $(this).toggleClass('active');
+    });
+
+    $(".payment-method ul li label").on('click', function () {
+        $(this).toggleClass('active');
+    });
+
+})(jQuery);
